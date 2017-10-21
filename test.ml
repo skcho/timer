8c0d9d008d890d6eed72fdd01e0ae482d8954f1a
skcho <skcho@ropas.snu.ac.kr>
2017-10-21 23:58:42 +0900

    Format

--- test.ml
+++ test.ml
@@ -2,61 +2,70 @@ let do_sth () =
   let rec f n = if n < 0 then () else f (n - 1) in
   f 500000000
 
+
 let simple () =
-  Timer.start "simple1";
-  do_sth ();
-  Timer.start "simple2";
-  do_sth ();
-  do_sth ();
-  Timer.end_ ();
+  Timer.start "simple1" ;
+  do_sth () ;
+  Timer.start "simple2" ;
+  do_sth () ;
+  do_sth () ;
+  Timer.end_ () ;
   ()
 
+
 let here () =
-  Timer.start_here [%here];
-  do_sth ();
-  Timer.start_here [%here];
-  do_sth ();
-  do_sth ();
-  Timer.end_ ();
+  Timer.start_here [%here ] ;
+  do_sth () ;
+  Timer.start_here [%here ] ;
+  do_sth () ;
+  do_sth () ;
+  Timer.end_ () ;
   ()
+
 
 let acc () =
   let f () =
-    Timer.acc_start_here [%here];
-    do_sth ();
-    Timer.acc_start_here [%here];
-    do_sth ();
-    do_sth ();
+    Timer.acc_start_here [%here ] ;
+    do_sth () ;
+    Timer.acc_start_here [%here ] ;
+    do_sth () ;
+    do_sth () ;
     Timer.acc_end ()
   in
-  List.iter f [(); ()];
+  List.iter f [(); ()] ;
   Timer.acc_flush ()
 
+
 module MyTimer1 = Timer.Make (struct
-    include Timer.Simple
-    let timer_name = "timer1"
-  end)
+  include Timer.Simple
+
+  let timer_name = "timer1"
+end)
 
 module MyTimer2 = Timer.Make (struct
-    include Timer.Simple
-    let timer_name = "timer2"
-  end)
+  include Timer.Simple
+
+  let timer_name = "timer2"
+end)
 
 let my_timer () =
-  MyTimer1.start_here [%here];
-  MyTimer2.start_here [%here];
-  do_sth ();
-  MyTimer1.end_ ();
-  do_sth ();
+  MyTimer1.start_here [%here ] ;
+  MyTimer2.start_here [%here ] ;
+  do_sth () ;
+  MyTimer1.end_ () ;
+  do_sth () ;
   MyTimer2.end_ ()
 
+
 let test title f =
-  prerr_endline ("TEST: "^title);
-  f ();
+  prerr_endline ("TEST: " ^ title) ;
+  f () ;
   prerr_newline ()
 
+
 let () =
-  test "simple timer" simple;
-  test "simple timer + ppx_here" here;
-  test "accumulate timer + ppx_here" acc;
+  test "simple timer" simple ;
+  test "simple timer + ppx_here" here ;
+  test "accumulate timer + ppx_here" acc ;
   test "multiple timers + ppx_here" my_timer
+
--- timer.ml
+++ timer.ml
@@ -28,29 +28,34 @@ module Make (T : InputS) : S = struct
   let print_time (name, t) =
     Printf.fprintf stderr "[%s] %s\t%.3f\n%!" T.timer_name name t
 
+
   let data_ref = ref T.init_data
 
   let end_ () =
-    (match T.get_time_end !data_ref with
-     | Some time -> print_time time
-     | None -> ());
+    ( match T.get_time_end !data_ref with
+    | Some time -> print_time time
+    | None -> () ) ;
     data_ref := T.mod_data_end !data_ref
 
+
   let start name =
-    end_ ();
+    end_ () ;
     data_ref := T.mod_data_start name !data_ref
 
+
   let flush () =
-    end_ ();
-    (match T.get_times_flush !data_ref with
-     | Some times -> List.iter print_time times
-     | None -> ());
+    end_ () ;
+    ( match T.get_times_flush !data_ref with
+    | Some times -> List.iter print_time times
+    | None -> () ) ;
     data_ref := T.init_data
+
 
   let start_here pos =
     let fname = pos.Lexing.pos_fname in
     let lnum = pos.Lexing.pos_lnum in
-    start (fname^":"^string_of_int lnum)
+    start (fname ^ ":" ^ string_of_int lnum)
+
 end
 
 module Simple = struct
@@ -68,49 +73,50 @@ module Simple = struct
     | None -> None
     | Some (name, start) -> Some (name, Sys.time () -. start)
 
+
   let get_times_flush _ = None
 end
 
 module Acc = struct
-
   let timer_name = "acc timer"
 
   module M = Map.Make (String)
 
-  type data = {
-    cur_opt : Simple.data;
-    all : float M.t;
-  }
+  type data = {cur_opt: Simple.data; all: float M.t}
 
-  let init_data = {
-    cur_opt = Simple.init_data;
-    all = M.empty
-  }
+  let init_data = {cur_opt= Simple.init_data; all= M.empty}
 
   let add_time name time all =
-    let all_time = try M.find name all with Not_found -> 0.0 in
+    let all_time =
+      try M.find name all
+      with Not_found -> 0.0
+    in
     M.add name (time +. all_time) all
+
 
   let add_cur_opt cur_opt all =
     match Simple.get_time_end cur_opt with
     | None -> all
     | Some (name, time) -> add_time name time all
 
+
   let mod_data_start title {cur_opt; all} =
     let all = add_cur_opt cur_opt all in
-    {cur_opt = Some (title, Sys.time ()); all}
+    {cur_opt= Some (title, Sys.time ()); all}
+
 
   let mod_data_end {cur_opt; all} =
     let all = add_cur_opt cur_opt all in
     let cur_opt = Simple.mod_data_end cur_opt in
     {cur_opt; all}
 
+
   let get_time_end _ = None
 
   let get_times_flush {all} =
-    M.fold (fun name time acc -> (name, time) :: acc) all []
-    |> List.rev
+    M.fold (fun name time acc -> (name, time) :: acc) all [] |> List.rev
     |> fun x -> Some x
+
 end
 
 module SimpleTimer = Make (Simple)
